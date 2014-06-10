1f5820bb1860e18cc851a9aec52efe5aaef7378e
David <david@Macintosh.local>
2014-06-10 10:07:17 +0200

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by David on 10.06.14.
+//  Copyright (c) 2014 DrSnickers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by David on 10.06.14.
+//  Copyright (c) 2014 DrSnickers. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
